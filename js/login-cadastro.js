6e5aab0108aa4fa4336aebed767375f15de2d7c8
ggabmartins <gabriellm1805@gmail.com>
2025-06-03 10:06:44 -0300

    Arquivo js do "login-cadastro" iniciado. Variaveis do formularios finalizadas.

---
+++ js/login-cadastro.js
@@ -0,0 +1,5 @@
+// variaveis dos fomularios
+const idIrPCadastro = document.getElementById('idIrPCadastro');
+const idIrPLogin = document.getElementById('idIrPLogin');
+const idFormLogin = document.getElementById('idFormLogin');
+const idFormCad = document.getElementById('idFormCad');
