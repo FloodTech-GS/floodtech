49aa6c17765c101b7c2da10e08dd8d61ad256ac1
ggabmartins <gabriellm1805@gmail.com>
2025-06-03 10:09:20 -0300

    Variaveis dos inputs finalizados.

--- js/login-cadastro.js
+++ js/login-cadastro.js
@@ -3,3 +3,12 @@ const idIrPCadastro = document.getElementById('idIrPCadastro');
 const idIrPLogin = document.getElementById('idIrPLogin');
 const idFormLogin = document.getElementById('idFormLogin');
 const idFormCad = document.getElementById('idFormCad');
+
+// variaveis dos inputs
+const idNome = document.getElementById('idNome');
+const idNumTel = document.getElementById('idNumTel');
+const idEmail = document.getElementById('idEmail');
+const idSenha = document.getElementById('idSenha');
+const idEmailCad = document.getElementById('idEmailCad');
+const idSenhaCad = document.getElementById('idSenhaCad');
+
