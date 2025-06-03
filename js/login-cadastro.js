345b2667b04f3509b9cf01dd2e8cf6e78e78b674
ggabmartins <gabriellm1805@gmail.com>
2025-06-03 10:14:09 -0300

    mudança de formularios finalizada.

--- js/login-cadastro.js
+++ js/login-cadastro.js
@@ -12,3 +12,20 @@ const idSenha = document.getElementById('idSenha');
 const idEmailCad = document.getElementById('idEmailCad');
 const idSenhaCad = document.getElementById('idSenhaCad');
 
+// mudança de formularios
+idIrPCadastro.addEventListener('click', () => {
+    idFormLogin.style.display = 'none';
+    idFormCad.style.display = 'flex';
+});
+
+idIrPLogin.addEventListener('click', () => {
+    idFormLogin.style.display = 'flex';
+    idFormCad.style.display = 'none';
+});
+
+let validNome = false;
+let validTelefone = false;
+let validEmail = false;
+let validSenha = false;
+let validEmailCad = false;
+let validSenhaCad = false;
