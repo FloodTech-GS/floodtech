63e2d53fae5d016cb5c3a6120618d307add9471b
pietroabrahamian <pietroabrahamian2018@gmail.com>
2025-06-04 21:03:59 -0300

    overflow hidden quando clicar no menu

--- js/menu.js
+++ js/menu.js
@@ -5,6 +5,7 @@ function toggleMenu () {
     nav.classList.toggle('active')
     const header = document.getElementById("header");
     header.classList.toggle('active-header')
+    document.body.style.overflow = 'hidden';
 }
 
 btnMobile.addEventListener("click", toggleMenu)
